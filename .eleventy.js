180446e497fc8304d3e73387e7c84039cd782879
eilume <eilume@protonmail.com>
2021-12-21 13:31:12 +0100

    feat: Add wip prismjs shortcode to replace 11ty plugin
    The reason for this change is that whilst the 11ty plugin offers great features (line highlighting, with added/removed line coloring), it massively bloats the HTML page size (although this is minimized with compression), which will quickly overtake the size of prism js. The big issue is that some of the features I'd like from prism don't appear to exist, such as added/removed line highlighting. I'll have a look again later but if it doesn't exist I'm sure I could hack/write my own simple system for this. I'm also going to replace the build system very soon since its _really_ bad haha

--- .eleventy.js
+++ .eleventy.js
@@ -115,8 +115,26 @@ const imageShortcode = async (
     return picture;
 };
 
+const highlightShortcode = (
+    content,
+    lang,
+    // showLinesNumbers = false,
+    addedLines = undefined,
+    removedLines = undefined
+) => {
+    if (addedLines != undefined) {
+        // Shift numbers by 1 to match the 11ty plugin by starting the first line starting at 0
+        let split = addedLines.split(', ');
+        split.forEach((v, i, a) => { outputArray = v.split('-'); outputArray.forEach((v2, i2, a2) => { v2 = (parseInt(v2) + 1).toString(); a2[i2] = v2; }); a[i] = outputArray.join('-') });
+        addedLines = split.join(', ');
+    }
+    // TODO: add support for removed lines somehow (make a plugin?)
+    // return `<pre${showLinesNumbers ? " class=" + '"' + "line-numbers" + '"' : ""}${addedLines != undefined ? " data-line=" + '"' + addedLines + '"' + " data-line-offset=" + '"' + "-1" + '"' : ""}><code class="language-${lang}">${content.replace(/\n/, "")}</code></pre>`;
+    return `<pre class="line-numbers language-${lang}"${addedLines != undefined ? " data-line=" + '"' + addedLines + '"' : ""}><code class="language-${lang}">${content.replace(/\n/, "")}</code></pre>`;
+};
+
 module.exports = function (config) {
-    config.addPlugin(pluginSyntaxHighlight);
+    // config.addPlugin(pluginSyntaxHighlight);
 
     // Date filter (localized)
     config.addNunjucksFilter("date", function (date, format, locale) {
@@ -132,15 +150,15 @@ module.exports = function (config) {
 
     config.setUseGitIgnore(true);
 
-    config.addWatchTarget('./src/assets/css/tailwind.css');
+    config.addWatchTarget("./src/assets/css/tailwind.css");
 
-    config.addPassthroughCopy('./src/assets/');
+    config.addPassthroughCopy("./src/assets/");
 
-    config.addPassthroughCopy('./src/robots.txt');
-    config.addPassthroughCopy('./src/_redirects');
+    config.addPassthroughCopy("./src/robots.txt");
+    config.addPassthroughCopy("./src/_redirects");
 
-    config.addShortcode('image', imageShortcode);
-    // config.addNunjucksAsyncShortcode('image', imageShortcode);
+    config.addShortcode("image", imageShortcode);
+    config.addPairedShortcode("highlight", highlightShortcode);
 
     return {
         passthroughFileCopy: true,
