7d6d2900d1192d86ac5185303e82106c64f82059
eilume <eilume@protonmail.com>
2022-01-01 20:19:21 +0100

    feat: Add vite compression plugin

--- build/vite.config.template.js
+++ build/vite.config.template.js
@@ -1,13 +1,17 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import { minifyHtml } from 'vite-plugin-html'
-import { VitePWA } from 'vite-plugin-pwa'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { minifyHtml } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
+import compress from 'vite-plugin-compress';
 
 export default defineConfig({
     root: "public",
     plugins: [
         minifyHtml(),
-        VitePWA()
+        VitePWA(),
+        compress({
+            brotli: false
+        })
     ],
     build: {
         outDir: "../dist",
